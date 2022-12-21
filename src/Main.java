8bb4c77287c7d5eaf1aa98ba60778803c37eba5e
keeptrynjoy <sungmin4218@gmail.com>
2022-12-22 01:21:41 +0900

    java의 정석 - 다형성 참조변수의 형변환, 참조변수와 인스턴스의 연결

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StringBuffer abc = new StringBuffer("abc");
-
-
-
-    }
-}
---
+++ src/java_jungsuk_ch7/BindingTest.java
@@ -0,0 +1,31 @@
+package java_jungsuk_ch7;
+
+
+class Parant1 {
+    int x = 100;
+
+    void method(){
+        System.out.println("Parant Method");
+    }
+}
+
+class Child1 extends Parant1 {
+    int x = 200;
+
+    void method(){
+        System.out.println("Child Method");
+    }
+}
+
+public class BindingTest {
+    public static void main(String[] args) {
+        Parant1 p = new Child1();
+        Child1 c = new Child1();
+
+        System.out.println("p.x = " + p.x);
+        p.method();
+        System.out.println();
+        System.out.println("c.x = " + c.x);
+        c.method();
+    }
+}
